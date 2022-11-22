f12ce12aba70ca6912cbed89b72120b5fbdf8326
Bennett Crowley <benjcrowley@gmail.com>
2022-11-22 13:22:02 -0700

    creating a library for books and their data

---
+++ part-3/part_3.py
@@ -0,0 +1,46 @@
+my_book = {
+    "title": "Then Hunger Games",
+    "author": "Suzanne Collins",
+    "year": 2008,
+    "rating": 4.32,
+    "pages": 374
+}
+
+# Follow the instructions in this part of the project. Define and flesh out your function below, which should accept a dictionary as an argument when called, and return a string of the info in that book-dictionary in a user-friendly readable format.
+
+# Code below
+def get_all():
+    pass
+
+
+
+# Once you are finished with that function, create several more functions which return individual pieces of information from the book.
+
+# Code below
+def get_title():
+    pass
+
+def get_author():
+    pass
+
+def get_year():
+    pass
+
+def get_rating():
+    pass
+
+def get_pages():
+    pass
+
+# Finally, create at least three new functions that might be useful as we expand our home library app. Perhaps think of a way you could accept additional arguments when the function is called? Also, imagine you have a list filled with dictionaries like above.
+
+# Code below
+
+def order_by_size(lst):
+    pass
+
+def print_books_by_author(lst):
+    pass
+
+def best_rated_book(lst):
+    pass
