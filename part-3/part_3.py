902d67bd59a72c7fa977d526b952d45203565c45
Bennett Crowley <benjcrowley@gmail.com>
2022-11-27 19:25:25 -0700

    added and input function and a main menu

--- part-3/part_3.py
+++ part-3/part_3.py
@@ -1,3 +1,4 @@
+import statistics
 my_book = {
     "title": "Then Hunger Games",
     "author": "Suzanne Collins",
@@ -5,42 +6,113 @@ my_book = {
     "rating": 4.32,
     "pages": 374
 }
+your_book = {
+    "title": "The Mocking Jay",
+    "author": "Suzanne Collins",
+    "year": 2010,
+    "rating": 4.425,
+    "pages": 370
+}
+their_book = {
+    "title": "Capitan Underpants",
+    "author": "Dav Pilkey",
+    "year": 1997,
+    "rating": 5.0,
+    "pages": 125
+}
+book_list = [my_book, your_book, their_book]
+book_list_2 = [
+    {
+    "title": "Then Hunger Games",
+    "author": "Suzanne Collins",
+    "year": 2008,
+    "rating": 4.32,
+    "pages": 374
+},
+{
+    "title": "The Mocking Jay",
+    "author": "Suzanne Collins",
+    "year": 2010,
+    "rating": 4.425,
+    "pages": 370
+},
+{
+    "title": "Capitan Underpants",
+    "author": "Dav Pilkey",
+    "year": 1997,
+    "rating": 5.0,
+    "pages": 125
+},
+{
+    "title": "Capitan Underpants",
+    "author": "Dav Pilkey",
+    "year": 1997,
+    "rating": 5.0,
+    "pages": 125
+}]
 
 # Follow the instructions in this part of the project. Define and flesh out your function below, which should accept a dictionary as an argument when called, and return a string of the info in that book-dictionary in a user-friendly readable format.
 
 # Code below
-def get_all():
-    pass
+def get_all(book):
+    '''pass in a dictionary, and return the title, author, year, rating, and number of pages'''
+    return f'This {book["pages"]} page book, titled {book["title"]}, by {book["author"]} in {book["year"]} is rated {book["rating"]}/5'
 
-
+print(get_all(my_book))
 
 # Once you are finished with that function, create several more functions which return individual pieces of information from the book.
 
 # Code below
-def get_title():
-    pass
+def get_title(book):
+    return book["title"]
 
-def get_author():
-    pass
+def get_author(book):
+    return book["author"]
 
-def get_year():
-    pass
+def get_year(book):
+    return book["year"]
 
-def get_rating():
-    pass
+def get_rating(book):
+    return book["rating"]
 
-def get_pages():
-    pass
+def get_pages(book):
+    return book["pages"]
 
 # Finally, create at least three new functions that might be useful as we expand our home library app. Perhaps think of a way you could accept additional arguments when the function is called? Also, imagine you have a list filled with dictionaries like above.
 
 # Code below
 
-def order_by_size(lst):
-    pass
+def most_published_author(lst):
+    author_list = []
+    res_str = ''
+    for i in range(len(lst)):
+        author_list.append(lst[i]["author"])
+        # print(author_list)
+    res = statistics.multimode(author_list)
+    if len(res) == 1:
+        return f'The most published author in this library is {res[0]}.'
+    elif len(res) == 2:
+        return f'The most published authors are {res[0]} and {res[1]}'
+    elif len(res) > 2:
+        return f'The most published authors are {res[0]}, {res[1]}, and {res[2]}'
 
-def print_books_by_author(lst):
-    pass
+    
+
+def get_author_set(lst):
+    author_set = set()
+    for i in range(len(lst)):
+        author_set.add(lst[i]["author"])
+    return author_set
 
 def best_rated_book(lst):
-    pass
+    best_book = "yeet"
+    highest_rating = 0
+    for i in range(len(lst)):
+        if lst[i]["rating"] > highest_rating:
+            highest_rating = lst[i]["rating"]
+            best_book = lst[i]["title"]
+    return f"{best_book}, rating: {highest_rating}"
+
+# print(get_author_set(book_list))
+# print(best_rated_book(book_list))
+print(most_published_author(book_list_2))
--- part-4/part_4.py
+++ part-4/part_4.py
@@ -1,33 +1,132 @@
+my_library = [
+    {
+    "title": "Then Hunger Games",
+    "author": "Suzanne Collins",
+    "year": 2008,
+    "rating": 4.32,
+    "pages": 374
+},
+{
+    "title": "The Mocking Jay",
+    "author": "Suzanne Collins",
+    "year": 2010,
+    "rating": 4.425,
+    "pages": 370
+},
+{
+    "title": "Capitan Underpants",
+    "author": "Dav Pilkey",
+    "year": 1997,
+    "rating": 5.0,
+    "pages": 125
+},
+{
+    "title": "Capitan Underpants",
+    "author": "Dav Pilkey",
+    "year": 1997,
+    "rating": 5.0,
+    "pages": 125
+}]
 ### Step 1 - Input function
 
 ## Create five input statements to gather user's book they want to input to the system. After that be sure to turn it into a function.
 
 # Code here
+def input_book_info():
+    title = input("What is the title of your book? ")
+    author = input("Who wrote your book? ")
+    year = input("what year did your book get published? ")
+    rating = input("What is your books rating out of 5? ")
+    pages = input("How many pages in your book? ")
+    new_book = {
+        "title": title,
+        "author": author,
+        "year": year,
+        "rating": rating,
+        "pages": pages
+    }
+    return new_book
 
-
+# print(input_book_info())
 ### Step 2 - Type conversion
 
 ## Now convert the proper data-types front strings to either floats or ints depending on what it is. Feel free to comment out your old function so you don't get an error, or copy/paste and give it a new name.
 
 # Code here
+# def enter_book_info():
+#     title = input("What is the title of your book? ")
+#     author = input("Who wrote your book? ")
+#     year = int(input("what year did your book get published? "))
+#     rating = float(input("What is your books rating out of 5? "))
+#     pages = int(input("How many pages in your book? "))
+#     new_book = {
+#         "title": title,
+#         "author": author,
+#         "year": year,
+#         "rating": rating,
+#         "pages": pages
+#     }
+#     return new_book
 
-
-
+# my_book = enter_book_info()
+# print(type(my_book["year"]))
+# print(type(my_book["title"]))
+# print(type(my_book["pages"]))
+# print(type(my_book["rating"]))
 ### Step 3 - Error handling
 
 ## Now take your previous function, and handle potential errors should the user type an answer that doesn't convert data-type properly.
 
 # Code here
+def enter_book_info():
+    title = input("What is the title of your book? ")
+    author = input("Who wrote your book? ")
+    try:
+        year = int(input("what year did your book get published? "))
+    except:
+        year = int(input("Please enter a year for the date"))
+    try:
+        rating = float(input("What is your books rating out of 5? "))
+    except:
+        rating = float(input("Please enter a valid rating "))
+    try:
+        pages = int(input("How many pages in your book? "))
+    except:
+        pages = int(input("Please enter a number for the # of pages "))
 
+    new_book = {
+        "title": title,
+        "author": author,
+        "year": year,
+        "rating": rating,
+        "pages": pages
+    }
+    return new_book
 
+# my_book = enter_book_info()
+# print(type(my_book["year"]))
+# print(type(my_book["title"]))
+# print(type(my_book["pages"]))
+# print(type(my_book["rating"]))
+# print(my_book)
 
 ### Step 4 - if/elif/else
 
 ## Now create a main menu function that gives the user options. Handle their choices with if/elif/else statements.
 
 # Code here
+def main_menu():
+    n = None
+    while n != 4:
+        n = int(input("Main Menu: \n 1. Add new book \n 2. See all books\n 3. Search by Title \n 4. Quit Program \n "))
+        if n == 1:
+            my_library.append(enter_book_info())
+        elif n == 2:
+            print(my_library)
+        elif n == 3:
+            print('Function not available')
 
-
+main_menu()
 ### Step 5 - while loops
 
 ## Now add a while loop to your main menu to keep it alive, and continually asking for input until the user chooses to exit the program. Call the main menu to ensure it functions properly.
